e1ad85b9aae4726f72925047bf85f7dd76222fd6
MarcosHenr1que <167825122+MarcosHenr1que@users.noreply.github.com>
2024-06-24 21:06:57 -0300

    Add files via upload

---
+++ ATIVIDADE_VCS .py
@@ -0,0 +1,7 @@
+print("Programa de repetição até digitar 0")
+
+user = int(input("Digite um número Inteiro"))
+while user != 0:
+    print("Diferente de zero!!!!")
+    user = int(input("Digite um número Inteiro"))
+print("Fim do programa")
